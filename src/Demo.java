41140d2c2754a529a62d8de0989459f8ddff3026
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-03-25 13:26:12 +0100

    Fix .gitignore implementation

---
+++ src/Demo.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+public class Demo {
+    public static void main(String[] args) {
+        Product p1 = new Product("Kaas", 400);
+        Product p2 = new Product("Wasmiddel", 800);
+        Product p3 = new Product("Worst", 200);
+        Product p4 = new Product("Chips", 400);
+        Product p5 = new Product("Appels", 150);
+
+        List<Product> producten = new ArrayList();
+        producten.add(p1);
+        producten.add(p2);
+        producten.add(p3);
+        producten.add(p4);
+        producten.add(p5);
+
+        for (Product p : producten) {
+            System.out.println(p.naam + ">>" + p.prijs);
+        }
+    }
+}
+
+class Product implements Comparable<Product> {
+    String naam;
+    int prijs;
+
+    Product(String naam, int prijs) {
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public String getNaam() {
+        return this.naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int getPrijs() {
+        return this.prijs;
+    }
+
+    public void setPrijs(int prijs) {
+        this.prijs = prijs;
+    }
+
+    @Override
+    public int compareTo(Product otherProduct) {
+        if (this.prijs < otherProduct.getPrijs()) {
+            return -1;
+        } else if (this.prijs > otherProduct.getPrijs()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (otherObject == null) {
+            return false;
+        }
+
+        if (this.getClass() != otherObject.getClass()) {
+            return false;
+        }
+
+        Product otherProduct = (Product) otherObject;
+
+        if (this.naam.equals(otherProduct.getNaam())
+                && this.prijs == otherProduct.getPrijs()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.naam, this.prijs);
+    }
+}
+
+class ProductComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        return p1.getNaam().compareTo(p2.getNaam());
+    }
+}
